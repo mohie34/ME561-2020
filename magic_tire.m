2b8e8efc7afe7213545328ca09f64a7c29c66346
tyz <zty181030@gmail.com>
2020-04-09 06:26:02 -0400

    model update
    magic tire and step5 to be completed

---
+++ magic_tire.m
@@ -0,0 +1,11 @@
+function [outputArg1,outputArg2] = magic_tire(inputArg1)
+%%This function calculates longitudinal and lateral force
+
+%Hard coded coefficients
+Bx = 10; Cx = 1.3; Dx = 1; Ex = 0.97;
+By = 1; Cy = 2; Dy = 3; Ey = 4;
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- vehicle_dynamics.m
+++ vehicle_dynamics.m
@@ -1,35 +1,51 @@
-function [states] = vehicle_dynamics(states_prev, inpt)
-%Write vehicle dynamics from the proposal's paper here. Pages 4-5-6
+function [vx_new, vy_new, vz_new, vxd_new, vyd_new, vzd_new,...
+          phi_new, theta_new, psi_new, phid_new, thetad_new, psid_new] ...
+        = vehicle_dynamics(vx, vy, vz, vxd, vyd, vzd,...
+                           phi, theta, psi, phid, thetad, psid,...
+                           torque, delta)
 
-% Mx = ...
-% My = ...
-% Mz = ...
+% % states:           vx, vy, vz, vxd, vyd, vzd, phi, theta, psi, phid, thetad, psid
+% %                   vx, velocity in x direction
+% %                   vxd, derivative of vx
+% %                   phi, theta, psi are rotation angle in x, y, z direction respectively              
+% % input:            torque, rear wheel torque
+% %                   delta, front wheel steering angle
 
-% For our trike, we follow fig 1-c p.4, Tr = 0.
-% Follow eq 19 - 27
+% Hard coded parameters
+g = 9.81;           % m/s^2
+h = 0.4;            % height of COG Affects PHI for roll
+Tf = 0.6;           % distance between front wheel
+Ms = 30; Mt = 40;   % vehicle sprung mass / total mass 
+lf = 0.3; lr = 0.4; % distance in x direction: front/rear wheel axis to COG
+Isxx = 0.1; Isyy = 0.2; Iszz = 0.3;  % Inertia to be estimated
 
-% F_i = tire force. 
-% Mt = total mass
-% Ms = Wheel moment of inertia
+%STEP1: Calculate Slip Angle (eq.25)
+alpha_fl = atan((vy+lf*psid)/(vx+Tf*psi/2))-delta; %front left
+alpha_fr = atan((vy+lf*psid)/(vx+Tf*psi/2))-delta; %front right
+alpha_r = atan((vy-lr*psid)/vx); %rear
 
-% use eq 7 to get X*F_fl, X*F_fr, and X*F_r
+%STEP2: Calculate Fzf and Fzr (reference: Rob535 HW2)
+Fzf = lf*Ms*g/(lf+lr);
+Fzr = lr*Ms*g/(lf+lr);
 
-% Things we have at each iteration
-% Constants = Mt, and Ms
-%             Isxx, Isyy, Iszz
-%             Tf, and Tr
-%             [IMPORTANT] h = height of COG Affects PHI for roll
-%             l_r and l_f = COG location on x-axis to rear and front
-% Control input = delta and T
-% States = vx, vy, vz and their dot
-%          psi, theta, phi and their dots
-%
-% Need to calculate accelaration (double dot) for these states at each 
-% iteration
+%STEP3: Use magic tire to calculate Fx and Fy for each tire
+Fxfl, Fyfl = magic_tire(alpha_fl, Fzf);
+Fxfr, Fyfr = magic_tire(alpha_fr, Fzf);
+Fxr, Fyr = magic_tire(alpha_r, Fzr);
 
-Mt*(vx_dot + theta_dot*v_z - vy*psi_dot) = X*F_fl + X*F_fr + X*F_r;
-Mt*(vy_dot + psi_dot*v_x - phi_dot*v_z) = Y*F_fl + Y_fr + Y*F_r;
-Ms*(vz_dot + psi_dot*v_y - theta_do*v_x) = F*z_fl + F*z_fr + F*z_r;
+%STEP4: Calculate XF and XY for each tire
+XFfl = Fxfl * cos(delta) - Fyfl * sin(delta);
+YFfl = Fyfl * cos(delta) + Fxfl * sin(delta);
+XFfr = Fxfr * cos(delta) - Fyfr * sin(delta);
+YFfr = Fyfr * cos(delta) + Fxfr * sin(delta);
+XFr = Fxr * cos(0) - Fyr * sin(0);
+YFr = Fyr * cos(0) + Fxr * sin(0);
+
+%STEP5: Estimate states tbd
+
+% Mt*(vx_dot + theta_dot*v_z - vy*psi_dot) = X*F_fl + X*F_fr + X*F_r;
+% Mt*(vy_dot + psi_dot*v_x - phi_dot*v_z) = Y*F_fl + Y_fr + Y*F_r;
+% Ms*(vz_dot + psi_dot*v_y - theta_do*v_x) = F*z_fl + F*z_fr + F*z_r;
 
 
 end
