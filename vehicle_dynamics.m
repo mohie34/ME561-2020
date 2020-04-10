6e33dff43b3919c32262bda5c2ca7e6264113e98
mohie <mohie@umich.edu>
2020-04-10 19:01:33 -0400

    Cleaned Vehicle Dynamics

--- vehicle_dynamics.m
+++ vehicle_dynamics.m
@@ -1,9 +1,8 @@
-function [vx_new, vy_new, vz_new, vxd_new, vyd_new, vzd_new,...
-          phi_new, theta_new, psi_new, phid_new, thetad_new, psid_new] ...
-        = vehicle_dynamics(vx, vy, vz, vxd, vyd, vzd,...
-                           phi, theta, psi, phid, thetad, psid,...
-                           torque, delta, ...
-                           timespan)
+function [vxd, vyd, vzd,...
+          phidd, thetadd, psidd] ...
+        = vehicle_dynamics(vx, vy, vz,...
+                           phid, thetad, psid,...
+                           Fx_r, delta)
 
 % % states:           vx, vy, vz, vxd, vyd, vzd, phi, theta, psi, phid, thetad, psid
 % %                   vx, velocity in x direction
@@ -13,16 +12,19 @@ function [vx_new, vy_new, vz_new, vxd_new, vyd_new, vzd_new,...
 % %                   delta, front wheel steering angle
 
 % Hard coded parameters
-g = 9.81;           % m/s^2
-h = 0.4;            % height of COG Affects PHI for roll
-Tf = 0.6;           % distance between front wheel
-Ms = 30; Mt = 40;   % vehicle sprung mass / total mass 
-lf = 0.3; lr = 0.4; % distance in x direction: front/rear wheel axis to COG
-Isxx = 0.1; Isyy = 0.2; Iszz = 0.3;  % Inertia to be estimated
+g = 9.81;           % m/s^2 [FINAL]
+h = 0.4;            % height of COG Affects PHI for roll [EXPERMINTAL]
+Tf = 0.86;          % distance between front wheel [m] [FINAL]
+Ms = 80; Mt = 89;   % vehicle sprung mass / total mass 
+r = 0.6; % [   | ]  % Ratio of front wheel to COG from total vehicle length
+l_tot = 1.5;        % Need to be final
+lf = r*l_tot; lr = (1-r)*l_tot; % distance in x direction: front/rear wheel axis to COG
+Isxx = 0.1; Isyy = 0.2; Iszz = 0.3;  % Inertia to be estimated [EXPERIMENTAL]
+                                     % TREAT IT AS A CUBE AND GET ROUGHT Inertai's
 
 %STEP1: Calculate Slip Angle (eq.25)
-alpha_fl = atan((vy+lf*psid)/(vx+Tf*psi/2))-delta; %front left
-alpha_fr = atan((vy+lf*psid)/(vx+Tf*psi/2))-delta; %front right
+alpha_fl = atan((vy+lf*psid)/(vx+Tf*psid/2))-delta; %front left
+alpha_fr = atan((vy+lf*psid)/(vx+Tf*psid/2))-delta; %front right
 alpha_r = atan((vy-lr*psid)/vx); %rear
 
 %STEP2: Calculate Fzf and Fzr (reference: Rob535 HW2)
@@ -45,23 +47,23 @@ YFr = Fyr * cos(0) + Fxr * sin(0);
 %STEP5a: Calculate double dot of phi, theta, psi (eq.22, 23, 24)
 phidd = (1/Isxx)*((Isyy-Iszz)*thetad*psid-(YFfl+YFfr+YFr)*h);
 thetadd = (1/Isyy)*((Iszz-Isxx)*thetad*psid+Fzr*lr-Fzf*lf+(XFfl+XFfr+XFr)*h);
-phidd = (1/Iszz)*((Isxx-Isyy)*phid*thetad+(XFfl-XFfr)*Tf/2+(YFfl+YFfr)*lf-YFr*lr);  % to add self-aligning torque Mzi
+psidd = (1/Iszz)*((Isxx-Isyy)*phid*thetad+(XFfl-XFfr)*Tf/2+(YFfl+YFfr)*lf-YFr*lr);  % to add self-aligning torque Mzi
 
 %STEP5b: Calculate dot of vx, vy, vz (eq.19, 20, 21)
-vxd_new = (XFfl+XFfr+XFr)/Mt-thetad*vz+psid*vy;
-vyd_new = (YFfl+YFfr+YFr)/Mt-psid*vx+phid*vz;
-vzd_new = (Fzf+Fzr)/Ms-phid*vy+thetad*vx;
+vxd = (XFfl+XFfr+XFr)/Mt-thetad*vz+psid*vy;
+vyd = (YFfl+YFfr+YFr)/Mt-psid*vx+phid*vz;
+vzd = (Fzf+Fzr)/Ms-phid*vy+thetad*vx;
 
 %STEP6: Update rest of the states
-vx_new = vx + timespan * vxd_new;
-vy_new = vy + timespan * vyd_new;
-vz_new = vz + timespan * vzd_new;
-phid_new = phid + timespan * phidd;
-thetad_new = thetad + timespan * thetadd;
-psid_new = psid + timespan * psidd;
-phi_new = phi + timespan * phid_new;
-theta_new = theta + timespan * thetad;
-psi_new = psi + timespan * psid;
+% vx_new = vx + timespan * vxd;
+% vy_new = vy + timespan * vyd;
+% vz_new = vz + timespan * vzd;
+% phid_new = phid + timespan * phidd;
+% thetad_new = thetad + timespan * thetadd;
+% psid_new = psid + timespan * psidd;
+% phi_new = phi + timespan * phid_new;
+% theta_new = theta + timespan * thetad;
+% psi_new = psi + timespan * psid;
 
 end
 
